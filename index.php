8e1ea5f4dd74123acdb795c6f86b351850af50d0
Iceheop <Iceheop@outlook.com>
2024-10-31 12:34:59 -0400

    Primer commit

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php include_once('includes/header.php')?>
+<!-- Inicio del contenido -->
+    <main class="w-100 vh-100 d-flex justify-content-center align-items-center">
+        <header class="container text-center" style="width: 550px;">
+            <h1>Login system</h1>
+            <p class="mb-2">Sistema de acceso con php + boostratp, donde se pueden resgistrar loguear usuarios, podiendo consulatar su información. </p>
+            <p><a href="#" class="me-2 link-offset-1">login</a><a href="#" class=" link-offset-1">registro</a></p>
+        </header>
+    </main>
+<!-- Fin del contenido -->
+<?php include_once('includes/footer.php')?>
